78b0034a9c8dbfd9e1d0e4d71eab9bc658b7d159
Hideaki Kanehara <hideaki.kanehara@gmail.com>
2020-04-17 01:00:11 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+notify-slack-status
+===
+
+## Deploying to Heroku
+
+```sh
+$ heroku create
+$ git push heroku master
+
+$ heroku run python manage.py migrate
+$ heroku open
+```
+or
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+## Documentation
+
+For more information about using Python on Heroku, see these Dev Center articles:
+
+- [Python on Heroku](https://devcenter.heroku.com/categories/python)
---
+++ app/main.py
@@ -0,0 +1,62 @@
+import os
+import json
+import requests
+from dotenv import load_dotenv
+from slackeventsapi import SlackEventAdapter
+from flask import Flask
+
+
+load_dotenv(verbose=True)
+
+WEBHOOK_URL = os.environ.get('WEBHOOK_URL', '')
+SLACK_SIGNING_SECRET = os.environ.get('SLACK_SIGNING_SECRET', '')
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, endpoint="/slack/events")
+
+
+def create_app():
+    app = Flask(__name__)
+    return app
+
+
+def post_by_webhook(text, username='events-app'):
+    response = requests.post(
+        WEBHOOK_URL,
+        data=json.dumps({
+            'text': text,
+            'username': username,
+            # 'icon_emoji': ':slack:'
+            'link_names': 1
+        }),
+        headers={'Content-Type': 'application/json'}
+    )
+    if response.status_code != 200:
+        raise ValueError(response.text)
+    print(response)
+
+
+@slack_events_adapter.on('user_change')
+def user_change(event_data):
+    '''Create an event listener for "user_change" events'''
+    event = event_data['event']
+    profile = event['user']['profile']
+    name = profile['display_name'] or profile['real_name']
+    text = f"{name}: {profile['status_emoji']} {profile['status_text']}"
+    print(text)
+    post_by_webhook(text)
+
+
+@slack_events_adapter.on('reaction_added')
+def reaction_added(event_data):
+    '''Create an event listener for "reaction_added" events'''
+    event = event_data['event']
+    emoji = event['reaction']
+    user = event['user']
+    # item_user = event['item_user'] or 'someone'
+    text = f'@{user} added a reaction :{emoji}:.'
+    print(text)
+    post_by_webhook(text)
+
+
+if __name__ == '__main__':
+    # Start the server on port 3000
+    slack_events_adapter.start(port=3000, debug=True)
